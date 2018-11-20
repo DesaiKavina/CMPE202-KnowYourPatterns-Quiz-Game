9b73b1ab1524c8f9a681860f70efd46b6bdf5ed9
vanireeya <reeya.vani@sjsu.edu>
2018-11-19 22:31:56 -0800

    Set flow for images to be fetched from model.

--- src/edu/sjsu/cmpe202/kyp/QuestionsPage.java
+++ src/edu/sjsu/cmpe202/kyp/QuestionsPage.java
@@ -30,50 +30,40 @@ public class QuestionsPage extends JFrame {
 
 	private JPanel contentPane;
 	private ButtonGroup btngrp;
-	static ArrayList<Model> quizQuestions = new ArrayList<>();
-	static int count;
+	public ArrayList<Model> quizQuestions = new ArrayList<>();
+	public int count, imageCounter;
 	String question;
-	JRadioButton rdbtnOption;
-	JRadioButton rdbtnOption_1;
-	JRadioButton rdbtnOption_2;
-	JRadioButton rdbtnOption_3;
+	
 	JLabel imageLabel;
+	JPanel panel;
 	Image image2;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					// question= quizQuestions.get(count).getQuestion();
-					// System.out.println("\n\n"+question);
-					// QuestionsPage frame = new QuestionsPage();
-					// frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	
+
+	// public String getQuestion() {
+	// question = this.quizQuestions.get(count).getQuestion();
+	// return question;
+	// }
+
+	public void setImage() throws IOException {
+		if (imageCounter >= 0 && imageCounter < 4) {
+			image2 = ImageIO.read(new File("src/resources/" + this.quizQuestions.get(count).getImages()[imageCounter]))
+					.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_SMOOTH);
+			imageLabel.setIcon(new ImageIcon(image2));
+		} else if (imageCounter == -1) {
+			image2 = ImageIO.read(new File("src/resources/Starter_img.jpg")).getScaledInstance(panel.getWidth(),
+					panel.getHeight(), Image.SCALE_SMOOTH);
+			imageLabel.setIcon(new ImageIcon(image2));
+		}
 	}
 
-	JLabel lblQuestions;
+	public void displayImage() throws IOException {
+		
 
-	public void setQuizObject(ArrayList<Model> arrayList) {
-		this.quizQuestions = arrayList;
-
+		setImage();
 	}
 
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	public void setScreen() {
-		setCount(count + 1);
-
-		imageLabel.setIcon(new ImageIcon(image2));
-	}
+	
 
 	/**
 	 * Create the frame.
@@ -81,6 +71,10 @@ public class QuestionsPage extends JFrame {
 	 * @throws IOException
 	 */
 	public QuestionsPage(ArrayList<Model> arrayList, int count) throws IOException {
+		quizQuestions = arrayList;
+		this.count = count;
+		imageCounter = -1;
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 692, 650);
 		contentPane = new JPanel();
@@ -95,14 +89,17 @@ public class QuestionsPage extends JFrame {
 		lblKnowYourPatterns.setBounds(229, 11, 290, 45);
 		contentPane.add(lblKnowYourPatterns);
 
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBounds(150, 67, 383, 235);
-		Image image = ImageIO.read(new File("src/resources/myImage.png")).getScaledInstance(panel.getWidth(),
+		Image image = ImageIO.read(new File("src/resources/Starter_img.jpg")).getScaledInstance(panel.getWidth(),
 				panel.getHeight(), Image.SCALE_SMOOTH);
-		ImageIcon imageIcon = new ImageIcon(image);
+		// ImageIcon imageIcon = new ImageIcon(image);
 		imageLabel = new JLabel(new ImageIcon(image));
 		panel.add(imageLabel);
 		contentPane.add(panel);
+
+		
+
 		//
 		// JLabel label = new JLabel();
 		// label.setIcon(new ImageIcon("myImage.PNG"));;
@@ -113,42 +110,38 @@ public class QuestionsPage extends JFrame {
 		// lblQuestions.setBounds(96, 362, 496, 73);
 		// contentPane.add(lblQuestions);
 
+
+
+
+
+
+
 		Button button = new Button("Hints");
-		Image image1 = ImageIO.read(new File("src/resources/1.png")).getScaledInstance(panel.getWidth(),
-				panel.getHeight(), Image.SCALE_SMOOTH);
-		image2 = ImageIO.read(new File("src/resources/2.png")).getScaledInstance(panel.getWidth(),
-				panel.getHeight(), Image.SCALE_SMOOTH);
+		// Image image1 = ImageIO.read(new
+		// File("src/resources/Adapter_1.jpg")).getScaledInstance(panel.getWidth(),
+		// panel.getHeight(), Image.SCALE_SMOOTH);
+		// image2 = ImageIO.read(new
+		// File("src/resources/Adapter_2.jpg")).getScaledInstance(panel.getWidth(),
+		// panel.getHeight(), Image.SCALE_SMOOTH);
 
 		button.addActionListener(new ActionListener() {
-			int clicked = 0;
 
 			public void actionPerformed(ActionEvent arg0) {
-				clicked++;
-				System.out.println("\n----------- The value of clicked counter is : " + clicked);
-				switch (clicked) {
-				case 1: {
-					System.out.println("Hints button clicked once");
-					imageLabel.setIcon(new ImageIcon(image1));
-					break;
+				imageCounter++;
+				try {
+					setImage();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				case 2: {
-					System.out.println("Hints button clicked twice");
-					imageLabel.setIcon(new ImageIcon(image2));
-					break;
-				}
-				default:
-					System.out.println("Default one");
-					break;
-				}
-				// System.out.println("hints btn clicked!!");
-				//
-				// imageLabel.setIcon(new ImageIcon(image1));
 
 			}
 		});
 		button.setBounds(466, 320, 70, 22);
 		contentPane.add(button);
 
+		
 
+		displayImage();
 	}
 }
